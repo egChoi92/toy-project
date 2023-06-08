6a6b632da3372ec424e618c45c015b08bf8cb7bf
egchoi <2013agal@gmail.com>
2023-06-08 21:00:59 +0900

    feat: json data 가져오기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Loading from "components/Loading";
+import Error from "components/Error";
 
-function App() {
+export default function App() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchTopic = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const response = await fetch("./data/topic.json");
+      const data = await response.json();
+      console.log('data: ', data);
+    } catch (error) {
+      setError(error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTopic();
+  }, []);
+
+  if (loading) return <Loading />;
+  if (error) return <Error />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+      </div>
   );
 }
-
-export default App;
