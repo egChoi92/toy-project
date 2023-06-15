e877816af5d51adf106c92a950b36747663cd1e3
FE_ChoiEungwang <2013agal@gmail.com>
2023-06-15 10:57:27 +0900

    feat: React Router 개발

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Signup from "pages/Signup";
+import Signin from "pages/Signin";
+import Todo from "pages/Todo";
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/todo" element={<Todo />} />
+        </Routes>
+    );
+};
+
+export default AppRoutes;
