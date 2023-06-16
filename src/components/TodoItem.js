cabd17a58d8efb5bbdf97bf0f973d651ae87675c
FE_ChoiEungwang <2013agal@gmail.com>
2023-06-16 15:01:13 +0900

    feat: Todo 아이템 텍스트 수정 구현

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,39 +1,51 @@
 import { updateTodoApi } from "api/todo";
 import TodoDelete from "components/TodoDelete";
 import TodoModify from "components/TodoModify";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-export default function TodoItem({todo, setTodoList}) {
+export default function TodoItem({ todo, setTodoList }) {
+  const [isModify, setIsModify] = useState(false);
   const checkboxRef = useRef(null);
   const todoTextRef = useRef(null);
   const todoId = todo.id;
-  
+  const todoText = todo.todo;
+
   const handleChecked = async () => {
-    const isCompleted = checkboxRef.current.checked
-    const todo = todoTextRef.current.textContent
+    const isCompleted = checkboxRef.current.checked;
+
+    const todo = isModify ? todoText :todoTextRef.current.textContent;
     const updateTodoData = {
       todo,
-      isCompleted
-    } 
+      isCompleted,
+    };
 
     const response = await updateTodoApi(todoId, updateTodoData);
     const modifyTodoItem = response.data;
 
+    if (isModify) {
+      setIsModify(false)
+    }
     setTodoList((prev) => {
       return prev.map((prevItem) => {
-        return prevItem.id === modifyTodoItem.id ? modifyTodoItem : prevItem      
-      })
-    })
-  }
+        return prevItem.id === modifyTodoItem.id ? modifyTodoItem : prevItem;
+      });
+    });
+  };
+
+  // useEffect(() => {
+  //   if (isModify) {
+  //     todoInputRef.current.focus();
+  //   }
+  // }, [isModify])
 
   return (
     <li>
       <label>
-        <input type="checkbox" ref={checkboxRef} defaultChecked={todo.isCompleted} onChange={handleChecked}/>
-        <span ref={todoTextRef}>{todo.todo}</span>
+        <input type="checkbox" ref={checkboxRef} defaultChecked={todo.isCompleted} onChange={handleChecked} />
+        {!isModify && <span ref={todoTextRef}>{todoText}</span>}
       </label>
-      <TodoModify />
-      <TodoDelete id={todoId} setTodoList={setTodoList}/>
+      <TodoModify todo={todo} isModify={isModify} setIsModify={setIsModify} setTodoList={setTodoList} />
+      <TodoDelete id={todoId} setTodoList={setTodoList} />
     </li>
   );
 }
--- src/components/TodoModify.js
+++ src/components/TodoModify.js
@@ -1,16 +1,47 @@
-import { useState } from "react";
+import { updateTodoApi } from "api/todo";
+import { useEffect, useRef } from "react";
 
-export default function TodoUtil() {
-  const [isModify, setIsModify] = useState(false);
+export default function TodoUtil({ todo, setTodoList, isModify, setIsModify }) {
+  const todoInputRef = useRef(null);
+  const todoId = todo.id;
+  const isCompleted = todo.isCompleted;
+
+  const toggleModifyMode = () => {
+    setIsModify(!isModify);
+  };
+
+  const handleModify = async () => {
+    const todo = todoInputRef.current.value;
+    const updateTodoData = {
+      todo,
+      isCompleted
+    }
+    const repsonse = await updateTodoApi(todoId, updateTodoData)
+    const newTodoItem = repsonse.data;
+    toggleModifyMode()
+    setTodoList(prev => {
+      return prev.map((prevItem) => prevItem.id === todoId? newTodoItem : prevItem)
+    })
+  }
+
+  useEffect(() => {
+    if (isModify) {
+      todoInputRef.current.focus();
+    }
+  }, [isModify]);
+
   return (
     <span>
-      <button data-testid="modify-button">수정</button>
-      {isModify && (
+      {isModify ? (
         <span>
-          <input data-testid="modify-input" />
-          <button data-testid="submit-button">제출</button>
-          <button data-testid="cancel-button">취소</button>
+          <input data-testid="modify-input" ref={todoInputRef} defaultValue={todo.todo} />
+          <button data-testid="submit-button" onClick={handleModify}>제출</button>
+          <button data-testid="cancel-button" onClick={toggleModifyMode}>취소</button>
         </span>
+      ) : (
+        <button data-testid="modify-button" onClick={toggleModifyMode}>
+          수정
+        </button>
       )}
     </span>
   );
