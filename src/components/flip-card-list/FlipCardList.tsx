59e5076227c301f4415efa43d5e6680665124ec2
egChoi92 <2013agal@gmail.com>
2024-07-13 16:21:16 +0900

    구현 3. 스크롤링 애니메이션

--- src/components/flip-card-list/FlipCardList.tsx
+++ src/components/flip-card-list/FlipCardList.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import * as S from "./styles";
 
 type FlipCard = {
@@ -15,21 +15,39 @@ const FlipCardList: FC<FlipCardListProps> = ({ imageData }) => {
 
   const handleClick = (index: number) => {
     setActive(index);
-    console.log(`Clicked card ${index}`);
   };
+
+  const [isVisible, setIsVisible] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => setIsVisible(entry.isIntersecting), { threshold: 0.5 });
+
+    const container = containerRef.current;
+    if (container) observer.observe(container);
+
+    return () => {
+      if (container) observer.unobserve(container);
+    };
+  }, []);
+
+  console.log("aaa", isVisible);
+
   return (
-    <S.CardList>
-      {imageData.map((data, index) => (
-        <S.Card key={index} onClick={() => handleClick(index)} isActive={active === index}>
-          <S.FrontImageContainer>
-            <S.Image src={data.frontImage.src} alt={data.frontImage.alt} />
-          </S.FrontImageContainer>
-          <S.BackImageContainer>
-            <S.Image src={data.backImage.src} alt={data.backImage.alt} />
-          </S.BackImageContainer>
-        </S.Card>
-      ))}
-    </S.CardList>
+    <S.Container>
+      <S.CardList ref={containerRef}>
+        {imageData.map((data, index) => (
+          <S.Card key={index} onClick={() => handleClick(index)} isActive={active === index} isVisible={isVisible}>
+            <S.FrontImageContainer>
+              <S.Image src={data.frontImage.src} alt={data.frontImage.alt} />
+            </S.FrontImageContainer>
+            <S.BackImageContainer>
+              <S.Image src={data.backImage.src} alt={data.backImage.alt} />
+            </S.BackImageContainer>
+          </S.Card>
+        ))}
+      </S.CardList>
+    </S.Container>
   );
 };
 
--- src/components/flip-card-list/styles.ts
+++ src/components/flip-card-list/styles.ts
@@ -1,30 +1,41 @@
 import styled from "@emotion/styled";
 
+export const Container = styled.div`
+  display: flex;
+  height: 150vh;
+`;
+
 export const CardList = styled.div`
   display: flex;
+  align-self: end;
   gap: 20px;
+  height: auto;
   perspective: 1000px;
 `;
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   isActive: boolean;
+  isVisible: boolean;
 }
-export const Card = styled.button<ButtonProps>(
-  ({ isActive }) => `
+export const Card = styled.button<ButtonProps>(({ isActive, isVisible }) => {
+  console.log(isVisible);
+
+  return `
     position: relative;
     width: 20vw;
     background-color: transparent;
     border: none;
     transform-style: preserve-3d;
-    transition: transform 0.6s;
+    transition: transform 0.6s, opacity 0.6s;
     aspect-ratio: 5 / 7;
     cursor: pointer;
     &:hover {
       ${!isActive && "transform: rotateY(60deg)"};
-    }
-    ${isActive && "transform: rotateY(180deg)"}
-  `
-);
+    };
+    ${isActive && "transform: rotateY(180deg)"};
+    ${!isVisible && "opacity: 0; transform: rotate3d(1, 1, 1, 45deg)"};
+  `;
+});
 
 const ImageContainer = styled.div`
   position: absolute;
